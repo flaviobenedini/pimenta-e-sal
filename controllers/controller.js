a7e77f00ab7d58a0c1d396a98d8ee0e3863aed2e
Flavio Benedini <flavio.benedini@me.com>
2022-05-05 15:57:46 -0300

    feat: adicionando o filtro do id no controller para o detalhe

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,10 +2,10 @@
 const cardapio = require('../models/cardapio')
 const path = require('path')
 
-
+ 
 const controller = {
     index: (req, res) => res.render('index', {cardapio: cardapio}),
-    detalhe: (req, res) => res.render('detalhe', {prato: cardapio.filter( item => item.id = req.params.id)}),
+    detalhe: (req, res) => res.render('detalhe', {prato: cardapio.filter( item => item.id == req.params.id)[0]}),
 }
 
 module.exports = controller
--- models/cardapio.js
+++ models/cardapio.js
@@ -19,7 +19,7 @@ const cardapio = [
         id: 3,
         nome: 'Mousse de arroz',
         descicao: 'Mousse de arroz com leite e aroma de flor de laranjeira',
-        descCompleta: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+        descCompleta: '',
         valor: 27.5,
         imagem: 'mousse-de-arroz-con-leite.jpg',
     },
@@ -29,7 +29,7 @@ const cardapio = [
         descicao: 'Aspargos brancos com vinagrete de legumes e presunto ibérico',
         descCompleta: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
         valor: 37.5,
-        imagem: 'aspargos.jpg',
+        imagem: 'aspargos.png',
     },
 ]
 
